11b1f1e2cb560722fecdfb347ec04a45886f421e
George Stephanis <daljo628@gmail.com>
2025-05-05 16:40:54 -0400

    Don't wrap the inner content for output.
    We don't need to add extra wrappers as that may mess up the markup and styling if something relies on direct child selectors.

--- src/restrict-block-content/save.js
+++ src/restrict-block-content/save.js
@@ -3,8 +3,6 @@ import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
 export default function save() {
 	return (
-		<div { ...useBlockProps.save() }>
-			<InnerBlocks.Content />
-		</div>
+		<InnerBlocks.Content />
 	);
 }
