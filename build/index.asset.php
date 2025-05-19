6b9faf9d3fe8ddcf5361717a4e0463181d75c888
George Stephanis <daljo628@gmail.com>
2025-05-19 11:30:52 -0400

    Build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-hooks', 'wp-i18n'), 'version' => 'bd4f8f11cb05d9089b05');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-hooks', 'wp-i18n'), 'version' => '894f4e5e0726d6e9cb4e');
